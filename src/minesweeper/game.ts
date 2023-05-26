e711ca8f7b4094475542d0984b6bc8069e396f15
Kevin Lee <kevinlee3627@gmail.com>
2023-05-26 09:58:14 -0500

    added Difficulty enum

--- src/minesweeper/game.ts
+++ src/minesweeper/game.ts
@@ -1,17 +1,29 @@
 import { Board } from "./board";
 
+export enum Difficulty {
+  BEGINNER,
+  INTERMEDIATE,
+  EXPERT,
+}
+
 export class Game extends EventTarget {
   board: Board;
   time = 0;
+  difficulty: Difficulty = Difficulty.BEGINNER;
 
   constructor(board: Board) {
     super();
     this.board = board;
   }
+
+  setDifficulty(difficulty: Difficulty): void {
+    this.difficulty = difficulty;
+  }
 }
 
 function main() {
   const board = new Board(16, 30, 99);
+  const game = new Game(board);
 }
 
 main();
