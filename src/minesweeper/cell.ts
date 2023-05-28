07bbbba4a141200eeaf230449f406befe5817edb
Kevin Lee <kevinlee3627@gmail.com>
2023-05-27 22:27:08 -0500

    De-duplication of code in reveal()

--- src/minesweeper/cell.ts
+++ src/minesweeper/cell.ts
@@ -48,19 +48,15 @@ export class Cell {
     if (this.isRevealed) return;
     if (this.isFlagged) return;
 
+    this.isRevealed = true;
+    this.elem.classList.add("revealed");
+    const revealEvent = new CustomEvent("reveal", { bubbles: true });
+    this.elem.dispatchEvent(revealEvent);
+
     if (this.minesAround === 0) {
-      this.isRevealed = true;
-      this.elem.classList.add("revealed");
-      const revealEvent = new CustomEvent("reveal", { bubbles: true });
-      this.elem.dispatchEvent(revealEvent);
       this.neighbors.forEach(cell => {
         cell.reveal();
       });
-    } else {
-      this.isRevealed = true;
-      this.elem.classList.add("revealed");
-      const revealEvent = new CustomEvent("reveal", { bubbles: true });
-      this.elem.dispatchEvent(revealEvent);
     }
   }
 
